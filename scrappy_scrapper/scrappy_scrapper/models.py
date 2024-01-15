a72d26cd3dab3ef340ff311725fc248b72addd82
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-01-15 15:09:28 -0600

    Attempt to update the models attributes

--- scrappy_scrapper/scrappy_scrapper/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-  item_name = models.CharField(max_length=255)
-  item_price = models.DecimalField(max_digits=100, decimal_places=2)
-  sale = models.BooleanField(default=False)
-  sale_percent = models.IntegerField(max_digits=3)
-  sale_price = models.DecimalField(max_digits=100, decimal_places=2)
